846dd177aee046b46b85edeef33222ce863dcfd6
wanderley florez <wander.flrz@gmail.com>
2024-10-01 19:35:40 -0500

    Actualizacion Modulo Registro

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './sign-in.css';
 
 function Register() {
     const [name, setName] = useState('');
+    const [lastname, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmpass, setConfirmPass] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -24,7 +26,8 @@ function Register() {
             if (response.ok) {
                 console.log('Registro exitoso', data)
             } else {
-                console.log('Error de Registro')
+                console.log('Error de Registro', data.errors[0].msg)
+                alert(`${data.errors[0].msg}`);
             }
 
         } catch (error) {
@@ -34,87 +37,105 @@ function Register() {
 
     const RegisterData = {
         name: name,
+        lastname: lastname,
         email: email,
-        password: password
+        password: password,
+        confirmpass: confirmpass
     };
 
 
 
     return (
-
-        <main className="form-signin">
-            <form onSubmit={handleSubmit}>
+        <div className=" justify-content-center vh-100 bg-success text-dark bg-opacity-25" >
+            <div className="p-1 container-fluid d-flex justify-content-center bg-black">
                 <img
-                    className="mb-4"
-                    src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg"
+                    className="align-items-center"
+                    src="../src/img/logo.jpeg"
                     alt="Bootstrap logo"
-                    width="72"
-                    height="57"
+                    width="80"
+                    height="70"
                 />
-                <h1 className="h3 mb-3 fw-normal">Registro</h1>
-                <div className="form-floating">
-                    <input
-                        type="email"
-                        className="form-control"
-                        id="floatingInput"
-                        placeholder="name@example.com"
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <label htmlFor="floatingInput">Nombre</label>
-                </div>
-                <div className="form-floating">
-                    <input
-                        type="email"
-                        className="form-control"
-                        id="floatingInput"
-                        placeholder="name@example.com"
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <label htmlFor="floatingInput">Apellido</label>
-                </div>
+                <span className="display-5 fw-bold text-success"
+                >DiambuPark</span>
+            </div>
+            <main className="form-signin d-flex justify-content-center m-5 p-5">
+                <form onSubmit={handleSubmit} className="w-25">
 
-                <div className="form-floating">
-                    <input
-                        type="email"
-                        className="form-control"
-                        id="floatingInput2"
-                        placeholder="name@example.com"
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <label htmlFor="floatingInput3">Usuario</label>
-                </div>
-                <div className="form-floating">
-                    <input
-                        type="password"
-                        className="form-control"
-                        id="floatingPassword"
-                        placeholder="Password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <label htmlFor="floatingPassword">Contraseña</label>
-                </div>
+                    <h1 className="h3 mb-3 fw-normal mt-5 text-center fw-bold">Registro</h1>
+                    <div className="form-floating mb-3">
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="floatingInput"
+                            placeholder="name@example.com"
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <label htmlFor="floatingInput">Nombre</label>
+                    </div>
+                    <div className="form-floating mb-3">
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="floatingInput1"
+                            placeholder="name@example.com"
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
+                        <label htmlFor="floatingInput">Apellido</label>
+                    </div>
 
-                <div className="form-check text-start my-3">
-                    <input
-                        className="form-check-input"
-                        type="checkbox"
-                        value="remember-me"
-                        id="flexCheckDefault"
-                    />
-                    {/* <label className="form-check-label" htmlFor="flexCheckDefault">
-                        Recuerdame
-                    </label> */}
-                </div>
-                <button className="btn btn-primary w-100 py-2" type="submit">
-                    Registrarse
-                </button>
-                <br></br>
-                <br></br>
-                {/* <button className="btn btn-primary w-100 py-2" type="submit">
+                    <div className="form-floating mb-3">
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="floatingInput2"
+                            placeholder="name@example.com"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <label htmlFor="floatingInput3">Email</label>
+                    </div>
+                    <div className="form-floating mb-3">
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="floatingPassword"
+                            placeholder="Password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <label htmlFor="floatingPassword">Contraseña</label>
+                    </div>
+                    <div className="form-floating mb-3">
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="floatingPassword1"
+                            placeholder="Password"
+                            onChange={(e) => setConfirmPass(e.target.value)}
+                        />
+                        <label htmlFor="floatingPassword">Confirmar Contraseña</label>
+                    </div>
+
+                    <div className="form-check text-start my-3">
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            value="remember-me"
+                            id="flexCheckDefault"
+                        />
+                        <label className="form-check-label" htmlFor="flexCheckDefault" aria-required>
+                            Acepto Terminos y Condiciones
+                        </label>
+                    </div>
+                    <button className="btn btn-success w-100 py-2" type="submit">
+                        Registrarse
+                    </button>
+                    <br></br>
+                    <br></br>
+                    {/* <button className="btn btn-primary w-100 py-2" type="submit">
                     Registrate
                 </button> */}
-            </form>
-        </main>
+                </form>
+            </main>
+        </div>
     );
 };
 
